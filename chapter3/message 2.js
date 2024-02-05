b96f5421809254594170643263aa95148cafbd9f
wnswo8628 <gbjwein1234@naver.com>
2024-02-05 17:46:12 +0900

    final # 16

---
+++ chapter3/message 2.js
@@ -0,0 +1,19 @@
+const getBoolRandom = (state, newNode) => {
+	const successLogin = state.bool
+
+	if (successLogin === false) {
+		newNode.style.color="red"
+		return '로그인이 실패했습니다.'
+	}
+	else if (successLogin === true) {
+		newNode.style.color="black"
+		return '로그인이 성공하였습니다.'
+	}
+	return "";
+}
+
+export default (target, { state }) => {
+	const newNode = target.cloneNode(true)
+	newNode.textContent = getBoolRandom(state, newNode)
+	return newNode
+}
---
+++ chapter4/index 2.js
@@ -0,0 +1,5 @@
+import login from './logincomponent.js'
+import select from './selectcomponent.js'
+
+window.customElements.define('login-component', Login)
+window.customElements.define('select-component', Select)
