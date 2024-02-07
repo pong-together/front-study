1c9ddec27adecc0eacbf60035d7982d06106bc58
vlssm <chrrvlssm@gmail.com>
2024-02-07 16:25:31 +0900

    chapter05 task2 #25

--- 05/index.js
+++ 05/index.js
@@ -1,11 +1,30 @@
 import notice from './notice.js'
 
+const Post = (result, property) => {
+	const newNode = document.createElement('p')
+	newNode.textContent = `${property}`
+	document.querySelector('.notice-list').appendChild(newNode)
+}
+
+const Posting = (result) => {
+	const title = `title : ${JSON.stringify(result.title)}`
+	const description = `description : ${JSON.stringify(result.description)}`
+	const created = `created : ${JSON.stringify(result.created)}`
+	const views = `view : ${JSON.stringify(result.views)}`
+
+	Post(result, title)
+	Post(result, description)
+	Post(result, created)
+	Post(result, views)
+}
+
 const output = (foreword, result) => {
-	for (let i = 0 ; i < result.length ; i++)
-	{
-		const newNode = document.createElement('p')
-		newNode.textContent = `${foreword} : ${JSON.stringify(result[i])}`
-		document.querySelector('div').appendChild(newNode)
+	const Node = document.querySelector('div')
+	Node.classList.add('notice-list')
+	Node.textContent = `${foreword}`
+	
+	for (let i = 0 ; i < result.length ; i++) {
+		Posting(result[i])
 	}
 }
 
